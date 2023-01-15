e9f364609073c3e695e870c9e1e0f9073647882e
Anatoliy Sharov <sharov.012@gmail.com>
2023-01-15 18:34:20 +0300

    feat: add slider & footer

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,9 @@
 window.addEventListener('DOMContentLoaded', () => {
     function openMenuHandler () {
         const hamburger = document.querySelector('.hamburger'),
-              menu = document.querySelector('.menu'),
-              header = document.querySelector('.header');
+              header = document.querySelector('.header'),
+              menu = header.querySelector('.menu');
+
 
             hamburger.addEventListener('click', () => {
                 hamburger.classList.toggle('hamburger_open');
@@ -15,8 +16,82 @@ window.addEventListener('DOMContentLoaded', () => {
                 } else {
                     menu.classList.remove('menu_open');
                     header.classList.remove('header_menu-open');
-                }
-            });
+            }
+        });
     }
+
+    function sliderHandler() {
+        const sliderWrapper = document.querySelector('.chech-up__wrapper'),
+              sliderInner = sliderWrapper.querySelector('.check-up__inner'),
+              slides = sliderWrapper.querySelectorAll('.card'),
+              prev = document.querySelector('.controls__arr_left'),
+              next = document.querySelector('.controls__arr_right'),
+              currentCount = document.querySelector('#current'),
+              totalCount = document.querySelector('#total'),
+              width = window.getComputedStyle(sliderWrapper).width;
+
+
+
+        function startInnerSlider () {
+
+            let slideIndex = 1,
+                offset = 0;
+
+            function deliteNoteDigits(str) {
+                return +str.replace(/\D/g, '');
+            }
+    
+            function showCurrentSlide (currSlide) {
+                currentCount.textContent = currSlide;
+            }
+    
+            totalCount.textContent = slides.length;
+    
+            sliderInner.style.width = 100 * slides.length + '%';
+    
+            next.addEventListener('click', () => {
+                if (offset >= (deliteNoteDigits(width) * slides.length - 1 + 1) - deliteNoteDigits(width)) {
+                    offset = 0;
+                    slideIndex = 0;
+                    currentCount.textContent = slideIndex;
+                } else {
+                    offset += deliteNoteDigits(width);
+                    currentCount.textContent = slideIndex;
+                }
+                sliderInner.style.transform = `translateX(-${offset}px)`;
+    
+                if (slideIndex == slides.length) {
+                    slideIndex = 1;
+                } else {
+                    slideIndex++;
+                }
+                showCurrentSlide (slideIndex);
+            });
+    
+            
+        prev.addEventListener('click', () => {
+            if (offset == 0) {
+                offset = deliteNoteDigits(width) * (slides.length - 1);
+            } else {
+                offset -= deliteNoteDigits(width);
+            }
+    
+            sliderInner.style.transform = `translateX(-${offset}px)`;
+    
+            if (slideIndex == 1) {
+                slideIndex = slides.length;
+            } else {
+                slideIndex--;
+            }
+            showCurrentSlide (slideIndex);
+        });
+        }
+
+        if (slides.length > 1) {
+            startInnerSlider();
+        }
+    }
+
+    sliderHandler();
     openMenuHandler ();
 });
